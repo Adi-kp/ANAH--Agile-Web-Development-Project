1a7d8f5873412f3ce9fc3b50140b2330374c0e3d
Aarya Bhorra <1302ab11@gmail.com>
2023-05-23 07:57:27 +0800

    unit testing

--- Flask-Project/test/test_routes.py
+++ Flask-Project/test/test_routes.py
@@ -8,7 +8,19 @@ def test_index_route(app, client):
 
 
 
+def test_chat_route_post(client):
+    message = 'Hello, world!'
+    response = client.post('/send_chat', json={'message': message})
+    data = response.get_json()
 
+    assert response.status_code == 200
+    assert data['status'] == 'success'
+    assert data['message'] == message
+
+def test_chat_route_invalid_method(client):
+    response = client.get('/chat')
+    assert response.status_code == 405
+    assert b'Method Not Allowed' in response.data
 
 
 # Add more tests for other routes and their expected behaviors
